3e4a081c8169dd081d55c0635a90537d8ec96579
yulyazbrv <zubareva.yo@gmail.com>
2023-06-10 12:24:22 +0300

    feat: change cors policy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,20 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+app.use(cors({
+  origin: "https://jwt-auth-client-mu.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  handlePreflightRequest: (req, res) => {
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "https://jwt-auth-client-mu.vercel.app",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE",
+      "Access-Control-Allow-Headers": "my-custom-header",
+      "Access-Control-Allow-Credentials": true,
+    });
+    res.end();
+  },
+}));
 app.use("/api", router);
 
 const start = async () => {
